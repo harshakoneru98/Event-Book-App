534fcf98767cf299aceabcc9e3715f91dcc7b5b0
Harsha Vardhan Koneru <hkoneru@usc.edu>
2022-03-05 02:14:50 -0800

    Added Navigation and Auth UI

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import { BrowserRouter, Route, Navigate, Routes } from 'react-router-dom';
-
+import MainNavigation from './components/Navigation/MainNavigation';
 import AuthPage from './pages/Auth';
 import BookingsPage from './pages/Bookings';
 import EventsPage from './pages/Events';
-
 import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 function App() {
+    const client = new ApolloClient({
+        cache: new InMemoryCache(),
+        uri: 'http://localhost:4000/graphql'
+    });
+
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Navigate to="/auth" />} />
-                <Route path="/auth" element={<AuthPage />} />
-                <Route path="/events" element={<EventsPage />} />
-                <Route path="/bookings" element={<BookingsPage />} />
-            </Routes>
-        </BrowserRouter>
+        <ApolloProvider client={client}>
+            <BrowserRouter>
+                <React.Fragment>
+                    <MainNavigation />
+                    <main className="main-content">
+                        <Routes>
+                            <Route path="/" element={<Navigate to="/auth" />} />
+                            <Route path="/auth" element={<AuthPage />} />
+                            <Route path="/events" element={<EventsPage />} />
+                            <Route
+                                path="/bookings"
+                                element={<BookingsPage />}
+                            />
+                        </Routes>
+                    </main>
+                </React.Fragment>
+            </BrowserRouter>
+        </ApolloProvider>
     );
 }
 
--- app/src/pages/Auth.js
+++ app/src/pages/Auth.js
@@ -1,7 +1,120 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useQuery, gql, useLazyQuery, useMutation } from '@apollo/client';
+
+const QUERY_LOGIN = gql`
+    query tokenGeneration($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            userId
+            token
+            tokenExpiration
+        }
+    }
+`;
+
+const CREATE_USER_MUTATION = gql`
+    mutation ($userInput: UserInput) {
+        createUser(userInput: $userInput) {
+            _id
+            password
+        }
+    }
+`;
 
 function AuthPage() {
-    return <h1>The Auth Page</h1>;
+    const [isEmail, setIsEmail] = useState('');
+    const [isPassword, setIsPassword] = useState('');
+    const [isLogin, setIsLogin] = useState(true);
+
+    const [fetchLogin, { data: loginData, error: loginError }] =
+        useLazyQuery(QUERY_LOGIN);
+
+    const [createUser] = useMutation(CREATE_USER_MUTATION);
+
+    let switchModeHandler = () => {
+        setIsLogin(!isLogin);
+    };
+
+    let submitHandler = async () => {
+        if (isEmail.trim().length === 0 || isPassword.trim().length === 0) {
+            return;
+        }
+        if (isLogin) {
+            fetchLogin({
+                variables: {
+                    email: isEmail,
+                    password: isPassword
+                }
+            });
+        } else {
+            await createUser({
+                variables: {
+                    userInput: {
+                        email: isEmail,
+                        password: isPassword
+                    }
+                }
+            })
+                .then((data) => {
+                    console.log(data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+    };
+
+    if (loginError) {
+        console.log(loginError);
+    }
+
+    if (loginData) {
+        console.log(loginData);
+    }
+
+    return (
+        <form
+            className="auth-form"
+            onSubmit={(e) => {
+                e.preventDefault();
+                submitHandler();
+            }}
+        >
+            <div className="form-control">
+                <label htmlFor="email">E-Mail</label>
+                <input
+                    type="email"
+                    id="email"
+                    onChange={(e) => {
+                        setIsEmail(e.target.value);
+                    }}
+                    value={isEmail}
+                />
+            </div>
+            <div className="form-control">
+                <label htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    onChange={(e) => {
+                        setIsPassword(e.target.value);
+                    }}
+                    value={isPassword}
+                />
+            </div>
+            <div className="form-actions">
+                <button type="submit">Submit</button>
+                <button
+                    type="button"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        switchModeHandler();
+                    }}
+                >
+                    Switch to {isLogin ? 'Signup' : 'Login'}
+                </button>
+            </div>
+        </form>
+    );
 }
 
 export default AuthPage;

9090888e99406eab3f5e4c1239016e90a3c03f9a
Harsha Vardhan Koneru <hkoneru@usc.edu>
2022-03-04 19:10:33 -0800

    Added Basic Routes

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Route, Navigate, Routes } from 'react-router-dom';
+
+import AuthPage from './pages/Auth';
+import BookingsPage from './pages/Bookings';
+import EventsPage from './pages/Events';
+
 import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <h1>It Works</h1>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navigate to="/auth" />} />
+                <Route path="/auth" element={<AuthPage />} />
+                <Route path="/events" element={<EventsPage />} />
+                <Route path="/bookings" element={<BookingsPage />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 

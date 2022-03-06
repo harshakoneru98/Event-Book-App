a8a6398934d8ac9b3589e2206734bdf487a489ed
Harsha Vardhan Koneru <hkoneru@usc.edu>
2022-03-05 16:18:22 -0800

    Added Basic Modal

--- app/src/pages/Events.js
+++ app/src/pages/Events.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Backdrop from '../components/Backdrop/Backdrop';
+import Modal from '../components/Modal/Modal';
+import './Events.css';
 
 function EventsPage() {
-    return <h1>The Events Page</h1>;
+    const [creating, setCreating] = useState(false);
+
+    let startCreateEventHandler = () => {
+        setCreating(true);
+    };
+
+    let modalConfirmHandler = () => {
+        setCreating(false);
+    };
+
+    let modalCancelHandler = () => {
+        setCreating(false);
+    };
+    return (
+        <React.Fragment>
+            {creating && <Backdrop />}
+            {creating && (
+                <Modal
+                    title="Add Event"
+                    canCancel
+                    canConfirm
+                    onCancel={modalCancelHandler}
+                    onConfirm={modalConfirmHandler}
+                >
+                    <p>Modal Content</p>
+                </Modal>
+            )}
+            <div className="events-control">
+                <p>Share your own Events!</p>
+                <button className="btn" onClick={startCreateEventHandler}>
+                    Create Event
+                </button>
+            </div>
+        </React.Fragment>
+    );
 }
 
 export default EventsPage;
